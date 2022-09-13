59f1acefb7c02a7f332a02bd16ff2cb78900d331
Vaisakh Nair <vbnair988@gmail.com>
2022-09-13 15:42:04 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Note from './Note'
+
+export default function 
+() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  )
+}
